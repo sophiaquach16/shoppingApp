8c3357b6a0147c2d84fcc925d71b2fc513dc8fa3
Stephanie Hallak <stephanie.hallak@hotmail.com>
2021-06-08 16:17:26 -0400

    product service with http client

--- src/app/Main/home/productsMock.ts
+++ src/app/Main/home/productsMock.ts
@@ -1,67 +1,67 @@
 export interface Product {
     id: number;
-    name: string;
+    title: string;
     price: number;
     description: string;
-    quantity: number;
-    imgUrl: string;
+    category: string;
+    image: string;
   }
   
-  export const products = [
-    {
-      id: 1,
-      name: 'Item1',
-      price: 799,
-      description: 'A large phone with one of the best screens',
-      quantity: 10,
-      imgUrl: 'https://cdn11.bigcommerce.com/s-hsi95a83fz/product_images/uploaded_images/5903-beach-shorts-orange-thumb.jpg?t=1585629450&_ga=2.88311448.2069277356.1585516345-1869486607.1578452111'
-    },
-    {
-      id: 2,
-      name: 'Item2',
-      price: 699,
-      description: 'A great phone with one of the best cameras',
-      quantity: 10,
-      imgUrl: 'https://cdn11.bigcommerce.com/s-hsi95a83fz/product_images/uploaded_images/5903-beach-shorts-orange-thumb.jpg?t=1585629450&_ga=2.88311448.2069277356.1585516345-1869486607.1578452111'
+  // export const products = [
+  //   {
+  //     id: 1,
+  //     name: 'Item1',
+  //     price: 799,
+  //     description: 'A large phone with one of the best screens',
+  //     quantity: 10,
+  //     imgUrl: 'https://cdn11.bigcommerce.com/s-hsi95a83fz/product_images/uploaded_images/5903-beach-shorts-orange-thumb.jpg?t=1585629450&_ga=2.88311448.2069277356.1585516345-1869486607.1578452111'
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'Item2',
+  //     price: 699,
+  //     description: 'A great phone with one of the best cameras',
+  //     quantity: 10,
+  //     imgUrl: 'https://cdn11.bigcommerce.com/s-hsi95a83fz/product_images/uploaded_images/5903-beach-shorts-orange-thumb.jpg?t=1585629450&_ga=2.88311448.2069277356.1585516345-1869486607.1578452111'
     
-    },
-    {
-      id: 3,
-      name: 'Item3',
-      price: 299,
-      description: '',
-      quantity: 10,
-      imgUrl: 'https://cdn11.bigcommerce.com/s-hsi95a83fz/product_images/uploaded_images/5903-beach-shorts-orange-thumb.jpg?t=1585629450&_ga=2.88311448.2069277356.1585516345-1869486607.1578452111'
+  //   },
+  //   {
+  //     id: 3,
+  //     name: 'Item3',
+  //     price: 299,
+  //     description: '',
+  //     quantity: 10,
+  //     imgUrl: 'https://cdn11.bigcommerce.com/s-hsi95a83fz/product_images/uploaded_images/5903-beach-shorts-orange-thumb.jpg?t=1585629450&_ga=2.88311448.2069277356.1585516345-1869486607.1578452111'
     
-    },
+  //   },
 
-    {
-      id: 4,
-      name: 'Item4',
-      price: 299,
-      description: '',
-      quantity: 10,
-      imgUrl: 'https://cdn11.bigcommerce.com/s-hsi95a83fz/product_images/uploaded_images/5903-beach-shorts-orange-thumb.jpg?t=1585629450&_ga=2.88311448.2069277356.1585516345-1869486607.1578452111'
+  //   {
+  //     id: 4,
+  //     name: 'Item4',
+  //     price: 299,
+  //     description: '',
+  //     quantity: 10,
+  //     imgUrl: 'https://cdn11.bigcommerce.com/s-hsi95a83fz/product_images/uploaded_images/5903-beach-shorts-orange-thumb.jpg?t=1585629450&_ga=2.88311448.2069277356.1585516345-1869486607.1578452111'
     
-    },
+  //   },
 
-    {
-      id: 5,
-      name: 'Item5',
-      price: 299,
-      description: '',
-      quantity: 10,
-      imgUrl: 'https://cdn11.bigcommerce.com/s-hsi95a83fz/product_images/uploaded_images/5903-beach-shorts-orange-thumb.jpg?t=1585629450&_ga=2.88311448.2069277356.1585516345-1869486607.1578452111'
+  //   {
+  //     id: 5,
+  //     name: 'Item5',
+  //     price: 299,
+  //     description: '',
+  //     quantity: 10,
+  //     imgUrl: 'https://cdn11.bigcommerce.com/s-hsi95a83fz/product_images/uploaded_images/5903-beach-shorts-orange-thumb.jpg?t=1585629450&_ga=2.88311448.2069277356.1585516345-1869486607.1578452111'
     
-    },
+  //   },
 
-    {
-      id: 6,
-      name: 'Item6',
-      price: 299,
-      description: '',
-      quantity: 10,
-      imgUrl: 'https://cdn11.bigcommerce.com/s-hsi95a83fz/product_images/uploaded_images/5903-beach-shorts-orange-thumb.jpg?t=1585629450&_ga=2.88311448.2069277356.1585516345-1869486607.1578452111'
+  //   {
+  //     id: 6,
+  //     name: 'Item6',
+  //     price: 299,
+  //     description: '',
+  //     quantity: 10,
+  //     imgUrl: 'https://cdn11.bigcommerce.com/s-hsi95a83fz/product_images/uploaded_images/5903-beach-shorts-orange-thumb.jpg?t=1585629450&_ga=2.88311448.2069277356.1585516345-1869486607.1578452111'
     
-    }
-  ];
+  //   }
+  // ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './SharedComponents/nav/nav.component';
@@ -27,7 +29,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

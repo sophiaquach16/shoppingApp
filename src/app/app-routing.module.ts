fa2929cbb62a25580edf35abe2c76597cead7fcc
sophiaquach16 <sophiaquach16@gmail.com>
2021-06-08 12:10:56 -0400

    checkout components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CheckoutComponent } from './Main/checkout/checkout.component';
 
 const routes: Routes = [
+  { path: 'checkout', component: CheckoutComponent },
 ];
 
 @NgModule({

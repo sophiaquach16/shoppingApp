32817c640a22d36fd0fd9904812804aff57999eb
Tansen Rahman <tansen.r@hotmail.com>
2021-06-09 11:26:12 -0400

    added pom.xml

---
+++ backend/src/test/java/com/shoppingApp/model/dao/ProductDaoImplTest.java
@@ -0,0 +1,36 @@
+package com.shoppingApp.model.dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductDaoImplTest {
+
+  @Test
+  void getProductById() {
+  }
+
+  @Test
+  void getAllProducts() {
+  }
+
+  @Test
+  void addProduct() {
+  }
+
+  @Test
+  void deleteProductById() {
+  }
+
+  @Test
+  void updateProduct() {
+  }
+
+  @Test
+  void addProductToCart() {
+  }
+
+  @Test
+  void deleteProductFromCart() {
+  }
+}
---
+++ backend/src/test/java/com/shoppingApp/model/dao/UserDaoImplTest.java
@@ -0,0 +1,36 @@
+package com.shoppingApp.model.dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDaoImplTest {
+
+  @Test
+  void getUserById() {
+  }
+
+  @Test
+  void getAllUsers() {
+  }
+
+  @Test
+  void addUser() {
+  }
+
+  @Test
+  void deleteUserById() {
+  }
+
+  @Test
+  void updateUser() {
+  }
+
+  @Test
+  void addUserCart() {
+  }
+
+  @Test
+  void addCartToUser() {
+  }
+}
